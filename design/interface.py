829c2f6092627dc3642a9f04f6916862b24e5cee
urasha <43294860+urasha@users.noreply.github.com>
2020-11-05 21:31:36 +1000

    Update interface.py

--- design/interface.py
+++ design/interface.py
@@ -1,1 +1,16 @@
-###
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
+
+
+class MainForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('model.ui', self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_form = MainForm()
+    main_form.show()
+    sys.exit(app.exec())
