dea0b24f9af6acbcb03192cd794dd735c3457c5a
urasha <43294860+urasha@users.noreply.github.com>
2020-11-06 00:00:50 +1000

    Add files via upload

--- design/interface.py
+++ design/interface.py
@@ -1,12 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5 import uic
 
 
-class MainForm(QWidget):
+class MainForm(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('model.ui', self)
+        uic.loadUi('main_window.ui', self)
 
 
 if __name__ == '__main__':
