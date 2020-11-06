f9fc171bd21ab695ff192ce6735bc7e29a48c5b1
urasha <43294860+urasha@users.noreply.github.com>
2020-11-07 00:29:55 +1000

    Add files via upload

--- gameplay/key_illumination.py
+++ gameplay/key_illumination.py
@@ -1,3 +1,7 @@
-class Illumination:
-    def __init__(self):
-        pass
+def highlight_key(key):
+    key.setStyleSheet('background: rgba(200, 100, 0, 0.3)')
+
+
+def stop_highlight(timer, key):
+    timer.stop()
+    key.setStyleSheet('background: none')
