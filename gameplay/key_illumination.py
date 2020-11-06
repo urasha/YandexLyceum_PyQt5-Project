e376ad57393b2ae79fd413ae1b8d70fcdcc5c446
urasha <43294860+urasha@users.noreply.github.com>
2020-11-07 00:36:21 +1000

    Delete key_illumination.py

--- gameplay/key_illumination.py
+++
@@ -1,7 +0,0 @@
-def highlight_key(key):
-    key.setStyleSheet('background: rgba(200, 100, 0, 0.3)')
-
-
-def stop_highlight(timer, key):
-    timer.stop()
-    key.setStyleSheet('background: none')
