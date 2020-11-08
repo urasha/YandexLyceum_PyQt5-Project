21b972657208c2adaf05d645246c610ae709db15
urasha <43294860+urasha@users.noreply.github.com>
2020-11-09 00:04:00 +1000

    Add files via upload

--- design/interface.py
+++ design/interface.py
@@ -10,19 +10,40 @@ class MainForm(QMainWindow):
         super().__init__()
         uic.loadUi('main_window.ui', self)
 
+        # constants
+        ILLUMINATION_TIME = 300
+
+        self.PIANO_KEYS = [self.label_2, self.label_3, self.label_4, self.label_5, self.label_6, self.label_7,
+                           self.label_8, self.label_9, self.label_10, self.label_11, self.label_12, self.label_13,
+                           self.label_14, self.label_15, self.label_16, self.label_17, self.label_18, self.label_19,
+                           self.label_20, self.label_21, self.label_22, self.label_23, self.label_24, self.label_25,
+                           self.label_26, self.label_27]
+
+        self.QT_KEYS = [Qt.Key_Q, Qt.Key_W, Qt.Key_E, Qt.Key_R, Qt.Key_T, Qt.Key_Y, Qt.Key_U, Qt.Key_I, Qt.Key_O,
+                        Qt.Key_P, Qt.Key_A, Qt.Key_S, Qt.Key_D, Qt.Key_F, Qt.Key_G, Qt.Key_H, Qt.Key_J, Qt.Key_K,
+                        Qt.Key_L, Qt.Key_Z, Qt.Key_X, Qt.Key_C, Qt.Key_V, Qt.Key_B, Qt.Key_N, Qt.Key_M]
+
+        # app actions
         self.actionClose_application_Ctrl_L.triggered.connect(qApp.quit)
 
+        # timer for illumination
         self.timer = QTimer(self)
-        self.timer.setInterval(300)
+        self.timer.setInterval(ILLUMINATION_TIME)
 
         self.illumination = Illumination(self.timer)
 
-        self.timer.timeout.connect(lambda: self.illumination.stop_highlight_key(self.label_2))  # тест на label_2
+        self.last_key = 0
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_1:  # key_1 для теста
+        if event.key() in self.QT_KEYS:
+            if self.timer.isActive():
+                self.PIANO_KEYS[self.last_key].setStyleSheet('background: none')
+
             self.timer.start()
-            self.illumination.highlight_key(self.label_2)  # тест на label_2
+            key_index = self.QT_KEYS.index(event.key())
+            self.last_key = key_index
+            self.timer.timeout.connect(lambda: self.illumination.stop_highlight_key(self.PIANO_KEYS[key_index]))
+            self.illumination.highlight_key(self.PIANO_KEYS[key_index])
 
 
 if __name__ == '__main__':
