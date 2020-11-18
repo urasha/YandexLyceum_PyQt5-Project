f1004bf79bc329a1beab054bcb9db8c42bc33121
urasha <43294860+urasha@users.noreply.github.com>
2020-11-18 23:11:27 +1000

    Add files via upload

---
+++ gameplay/point_analysis.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+class PointAnalysis:
+    def __init__(self):
+        self.average = 0
+        self.max = 0
+
+    def open(self):
+        self.bd = sqlite3.connect("../design/scoreDB.db")
+        self.cur = self.bd.cursor()
+
+    def max_score(self):
+        req = 'SELECT MAX(score) FROM all_score'
+        result = self.bd.execute(req)
+        return result.fetchone()[0]
+
+    def averages_score(self):
+        req = 'select avg(score) from all_score'
+        result = self.bd.execute(req)
+        return result.fetchone()[0]
+
+    def recording_score(self, score):
+        req = f"""INSERT INTO all_score(score) VALUES({score})"""
+        self.bd.execute(req)
+
+    def close(self):
+        self.bd.commit()
+        self.bd.close()
--- gameplay/random_list.py
+++ gameplay/random_list.py
@@ -1,16 +1,12 @@
-def create_random_list(language):
+def create_random_list():
     from random import choice
 
-    fridge_random = list()
-    letters = ''
     NUMBER_OF_LETTERS = 25
 
-    if language == 'ru':
-        letters = 'йцукенгшщзхъфывапролджэячсмитьбю'
-    elif language == 'en':
-        letters = 'abcdefghijklmnopqrstuvwxyz'
+    fridge_random = list()
+    letters = 'abcdefghijklmnopqrstuvwxyz'
 
     for letter in range(NUMBER_OF_LETTERS):
-        fridge_random.append(choice(letters))
+        fridge_random.append(choice(letters).upper())
 
     return fridge_random
